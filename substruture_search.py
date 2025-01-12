9fffbe6114238fc6411cb3ebecb8048efa5788f9
Farzad <Farzadmst94@gmail.com>
2025-01-12 16:13:05 -0330

    Virtual env

--- substruture_search.py
+++ substruture_search.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from rdkit import Chem
 from rdkit.Chem import Draw
-from rdkit.Chem import PandasTools
 import pandas as pd
 st.image("SubStruture_Search.png",use_container_width=True)
 
